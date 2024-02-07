cc8a11aa5a783a13d4023f68e2905ff0298e1dd4
Julius Bendt <julius@juto.dk>
2024-02-07 19:15:20 +0100

    Removed header (closes #14 ), Moved footer into its own components, Fixed issue where the background would turn white, if to many registrations had been formatted (closes #12)

--- src/HomeView.vue
+++ src/HomeView.vue
@@ -1,12 +1,7 @@
 <template>
-  <div class="p-4 h-screen w-screen bg-background text-text">
-    <div class="flex justify-between">
-      <h1 class="text-xl font-bold">Hjælper til tidsregistrering</h1>
-    </div>
+  <div class="p-4 min-h-screen w-screen bg-background text-text">
     <main class="grid grid-cols-2 mt-2 gap-4">
       <div>
-        <h2 class="text-lg font-bold mb-2">Register</h2>
-
         <textarea
           rows="24"
           v-model="registrationsText"
@@ -23,7 +18,6 @@ Husk du kan klikke på en besked under 'formatteret' for at kopiere denne til cl
         ></textarea>
       </div>
       <div>
-        <h2 class="text-lg font-bold mb-2">Formatteret</h2>
         <div v-if="errors.length == 0">
           <p class="font-bold pt-3">Total: {{ calculateTotalTime(registrationsArray) }}</p>
           <RegistrationTable v-if="errors.length == 0" :registrations="registrationsArray" />
@@ -32,19 +26,7 @@ Husk du kan klikke på en besked under 'formatteret' for at kopiere denne til cl
         <ErrorTable v-else :errors="errors" />
       </div>
     </main>
-    <footer class="mt-10">
-      <p class="text-xs text-center">
-        Crafted with passion ❤️ and a relentless pursuit of perfection by the dynamic duo
-        <br />
-        <a href="https://jub.dk" class="text-primary" target="_blank">Julius Bendt</a>
-        and
-        <a href="https://github.com/neophear" class="text-primary" target="_blank">Stiig Gade</a>
-        <br />
-        <a href="https://github.com/Julius-Bendt/tid.jub.dk" class="text-primary" target="_blank">
-          Github repository
-        </a>
-      </p>
-    </footer>
+    <FooterNav />
   </div>
 </template>
 
@@ -61,6 +43,7 @@ import {
 
 import RegistrationTable from '@/components/RegistrationTable.vue'
 import ErrorTable from '@/components/ErrorTable.vue'
+import FooterNav from '@/components/FooterNav.vue'
 
 const registrationsText = ref(loadFromStorage()) // The panel to the left
 // If any string was found in the cache, format it
@@ -143,9 +126,7 @@ function setOrAddRegistration(input: IRegistration) {
 
     // If the old registration has a description, and the new registration also has a description, add an error
     if (!!oldRegistration.description && !!input.description) {
-      errors.value.push(
-        `Opgaven med id'et '${input.letter}' har allerede en beskrivelse`
-      )
+      errors.value.push(`Opgaven med id'et '${input.letter}' har allerede en beskrivelse`)
       return
     }
 
